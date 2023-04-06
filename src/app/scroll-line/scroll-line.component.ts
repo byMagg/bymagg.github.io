d80945eab78fa31c9faee3a3a174733cf0b08737
byMagg <byMag@hotmail.es>
2023-04-06 21:56:24 +0200

    fix scroll with image and fixed position

--- src/app/scroll-line/scroll-line.component.ts
+++ src/app/scroll-line/scroll-line.component.ts
@@ -13,7 +13,8 @@ export class ScrollLineComponent {
 
   @Input() maxHeight: number = 500;
   @Input() scrollInitialPosition: number = 500
-  height = 0
+  height: number = 0
+  display: boolean = false;
 
   constructor() { }
 
@@ -21,10 +22,12 @@ export class ScrollLineComponent {
     console.log(ev.detail.currentY)
     if (ev.detail.currentY > this.maxHeight) {
       this.height = (ev.detail.currentY - this.maxHeight) / this.scrollInitialPosition
-      console.log(this.height)
+      this.display = true
     } else if (ev.detail.currentY <= this.maxHeight) {
       this.height = 0
+      this.display = false
     } else {
+      this.display = true
       this.height = this.maxHeight
     }
   }
